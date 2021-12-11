ab0b5944349b4b4dc6b4b94ba180e06f165eff9e
jasminejuvyns <95414537+jasminejuvyns@users.noreply.github.com>
2021-12-11 14:44:27 +0100

    slideshow index

---
+++ script.js
@@ -0,0 +1,23 @@
+//slideshow
+
+
+var myIndex = 0;
+content();
+
+function content() {
+    try{
+        var i;
+        var x = document.getElementsByClassName("slides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }    
+        x[myIndex-1].style.display = "block";
+    }
+    catch(err) {
+    }
+    setTimeout(content, 2500); 
+}
