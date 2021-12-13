bd6d47d175b406ae3871318a5e9a2346bd2daea6
jasminejuvyns <95414537+jasminejuvyns@users.noreply.github.com>
2021-12-13 20:13:07 +0100

    javascript

---
+++ slideshow.js
@@ -0,0 +1,24 @@
+//-----------------SLIDESHOW--------------------------
+"use strict";
+let myIndex = 0;
+content();
+
+function content() {
+    try{
+        let i;
+        let x = document.getElementsByClassName("slides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }    
+        x[myIndex-1].style.display = "block";
+    }
+    catch(err) {
+    }
+    setTimeout(content, 2500); 
+}
+
+
