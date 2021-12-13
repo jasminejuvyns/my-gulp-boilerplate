46157ac51a583da800c17d7e56304f8fb4062298
jasminejuvyns <95414537+jasminejuvyns@users.noreply.github.com>
2021-12-13 09:26:16 +0100

    script: reserveringsformulier

--- script.js
+++ script.js
@@ -44,3 +44,106 @@ function content() {
         }
 
         //---------  EINDE SCROLL TO TOP -------------//
+
+
+//----------------RESERVERINGSFORMULIER----------------------------------------------
+//-----------------HOOFDSCRIPT------------
+let voornaamTxt;
+let achternaamTxt;
+let emailTxt;
+let telefoonTxt;
+
+let allesCorrectIngevuld=true;
+
+//-------FUNCTIE  VOORNAAM --------------------------------------------------------
+function controleerVoorwaardenVoornaam() {
+    if (voornaamTxt.length < 2){
+        document.getElementById("voornaam_error").innerHTML="Minstens 2 karakters lang!";
+        allesCorrectIngevuld = false;
+    }
+    else{
+        document.getElementById("voornaam_error").innerHTML="";
+    }
+}
+
+//-------FUNCTIE  ACHTERNAAM --------------------------------------------------------
+function controleerVoorwaardenAchternaam(){
+    if(achternaamTxt.length < 2 ){ 
+      document.getElementById("achternaam_error").innerHTML="Minstens 2 karakters lang!";
+      allesCorrectIngevuld = false;
+    }
+    else{
+      document.getElementById("achternaam_error").innerHTML="";
+    }
+}
+
+//-------FUNCTIE  EMAIL --------------------------------------------------------
+function controleerVoorwaardenEmail(){
+    let regExp=/^[A-Za-z][\.A-Za-z0-9+_-]+@[\.A-Za-z0-9-]+\.[A-Za-z]{2,20}$/;
+    if(regExp.test(emailTxt) == false){
+        document.getElementById("email_error").innerHTML="Dit is niet correct!";
+        allesCorrectIngevuld = false;
+    }
+    else{
+        document.getElementById("email_error").innerHTML="";
+    }
+}
+
+//-------FUNCTIE  TELEFOONNUMMER --------------------------------------------------------
+function controleerVoorwaardenEmail(){
+    let regExp=/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/;
+    if(regExp.test(telefoonTxt) == false){
+        document.getElementById("telefoonnummer_error").innerHTML="Dit is niet correct!";
+        allesCorrectIngevuld = false;
+    }
+    else{
+        document.getElementById("telefoonnummer_error").innerHTML="";
+    }
+}
+
+
+
+
+
+
+
+//-----------------FUNCTIE----------------
+function verstuur() {
+    voornaamTxt=document.getElementById("voornaam").value;
+    achternaamTxt=document.getElementById("achternaam").value;
+    emailTxt=document.getElementById("email").value;
+    telefoonTxt=document.getElementById("telefoonnummer").value;
+    if (voornaamTxt.length==0){
+        document.getElementById("voornaam_error").innerHTML="Vul in a.u.b.";
+        //uit de span in html
+        allesCorrectIngevuld = false;
+    }
+    else{
+        controleerVoorwaardenVoornaam();
+    }
+    if(achternaamTxt.length==0){
+        document.getElementById("achternaam_error").innerHTML="Vul in a.u.b.";
+        allesCorrectIngevuld = false;
+    }
+    else{
+        controleerVoorwaardenAchternaam();
+    }
+    if(emailTxt.length==0){
+        document.getElementById("email_error").innerHTML="Vul in a.u.b.";
+        allesCorrectIngevuld = false;
+    }
+    else{
+        controleerVoorwaardenEmail();
+    }
+    if(telefoonTxt.length==0){
+        document.getElementById("telefoonnummer_error").innerHTML="Vul in a.u.b.";
+        allesCorrectIngevuld = false;
+    }
+    else{
+        controleerVoorwaardenTelefoon();
+    }
+
+    //uiteindelijk controleren of alles correct is
+    //deze if blijft altijd allerlaatst in de verstuur() functie
+    if (allesCorrectIngevuld) {
+        document.write("Naam is correct ingevuld");
