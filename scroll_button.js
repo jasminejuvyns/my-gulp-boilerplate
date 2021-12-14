d8fae7698a437033bee10de79c2aa9c7851ca4cf
jasminejuvyns <95414537+jasminejuvyns@users.noreply.github.com>
2021-12-14 12:12:58 +0100

    update javascript

--- scroll_button.js
+++ scroll_button.js
@@ -1,20 +1,21 @@
 //---------------  BEGIN SCROLL TO TOP --------------------------------
-//Get the button:
-mybutton = document.getElementById("myBtn");
+"use strict"
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+mybutton = document.getElementById("myBtn");
+window.onscroll = function () { 
+    scrollFunction() 
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
-    } else {
+    } 
+    else {
         mybutton.style.display = "none";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // voor Safari
+    document.documentElement.scrollTop = 0; // voor Chrome, Firefox en IE
 }
